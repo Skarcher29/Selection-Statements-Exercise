2078d8c0c99bd24636d680d830aae534a828b3e3
Skarcher29 <samanthakarcher@gmail.com>
2023-11-16 19:20:56 -0600

    exercise part 1 finished

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SelectionStatementExercise
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess the right number!");
+            Console.WriteLine("What is the answer to Life, The Universe, and Everything?");
+
+            int theAnswer = 42;
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < theAnswer)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if(userInput > theAnswer)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
         }
     }
 }
